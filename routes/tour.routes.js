10d5a17eace9d928ebc1e4b58acaa0c445fb20bf
LozanoTheStain <adslozanov@gmail.com>
2022-11-21 23:02:16 -0500

    Añadiendo nueva validación

--- routes/tour.routes.js
+++ routes/tour.routes.js
@@ -9,11 +9,12 @@ router.post(
   '/',
   [
     validateJWT,
-    isRole("ADMIN_ROLE", "GUIE_ROLE"),
+    isRole('ADMIN_ROLE', 'GUIE_ROLE'),
     check('name', 'El nombre es requerido').not().isEmpty(),
     check('description', 'La description es requerida').not().isEmpty(),
     check('route', 'La ruta es requerida').not().isEmpty(),
     check('meetingPlace', 'El lugar de encuentro es requerido').not().isEmpty(),
+    check('guie', 'El guia es requerido').not().isEmpty(),
     check('paymentNetwork', 'El metodo de pago es requerido').not().isEmpty(),
     validateFields,
   ],

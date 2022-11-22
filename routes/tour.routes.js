504b787adb9e66416be0f14bf4c74dcc49abfc02
LozanoTheStain <adslozanov@gmail.com>
2022-11-21 22:49:43 -0500

    Añadiendo validaciones

--- routes/tour.routes.js
+++ routes/tour.routes.js
@@ -1,9 +1,23 @@
 const { Router } = require('express')
 const { check } = require('express-validator')
 const { createTour } = require('../controllers')
+const { validateFields, validateJWT, isRole } = require('../middlewares')
 
 const router = Router()
 
-router.post('/', createTour)
+router.post(
+  '/',
+  [
+    validateJWT,
+    isRole("ADMIN_ROLE", "GUIE_ROLE"),
+    check('name', 'El nombre es requerido').not().isEmpty(),
+    check('description', 'La description es requerida').not().isEmpty(),
+    check('route', 'La ruta es requerida').not().isEmpty(),
+    check('meetingPlace', 'El lugar de encuentro es requerido').not().isEmpty(),
+    check('paymentNetwork', 'El metodo de pago es requerido').not().isEmpty(),
+    validateFields,
+  ],
+  createTour
+)
 
 module.exports = router

f7c31d630617037b068aeceec5ba53ddad3b336b
LozanoTheStain <adslozanov@gmail.com>
2023-01-09 22:08:49 -0500

    Actualizando modelo y controlador

--- controllers/tour.controller.js
+++ controllers/tour.controller.js
@@ -1,19 +1,41 @@
 const { request, response } = require('express')
 const { DateTime } = require('luxon')
+const { Tour } = require('../models')
 
 const createTour = async (req = request, res = response) => {
-    try {
-        res.json({
-            ok: 'True'
-        })
-    } catch (error) {
-        console.log(error)
-        res.status(500).json({
-            msg: 'Error en el servidor'
-        })
+  try {
+    let { name, ...body } = req.body
+    name = name.toLowerCase().trim()
+
+    const tourBD = await Tour.findOne({ name })
+    if (tourBD) {
+      return res.status(400).json({
+        msg: `Ya existe un artículo con el titulo ${title}`,
+      })
     }
+    
+    data = {
+      ...body,
+      name: '',
+      description: '',
+      user: req.authenticatedUser.id,
+      data: Date.now(),
+      route: [],
+      meetingPlace: [],
+      paymentNetwork: [],
+    }
+
+    res.json({
+      ok: 'True',
+    })
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({
+      msg: 'Error en el servidor',
+    })
+  }
 }
 
 module.exports = {
-    createTour,
+  createTour,
 }
